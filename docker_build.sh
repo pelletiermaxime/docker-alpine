d61a5394f73a31e991249088f1c3b8c5d9dd9dc8
Maxime Pelletier <pelletiermaxime@gmail.com>
2015-05-10 13:24:11 -0400

    Move Dockerfiles to their own directory to be able to use the automated builds of the registry

--- docker_build.sh
+++ docker_build.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-docker build -f DockerfilePHP-FPM -t alpine-php-fpm .
-docker build -f DockerfileNginx -t alpine-nginx .
+docker build -f PHP-FPM/Dockerfile -t alpine-php-fpm .
+docker build -f Nginx/Dockerfile -t alpine-nginx .
